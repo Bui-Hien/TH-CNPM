d219e1a25bace5d62fc9cb4a00c67e3e8976308c
Bui-Hien <112457611+Bui-Hien@users.noreply.github.com>
2022-09-28 14:52:29 +0700

    completeinterface

---
+++ js.js/area.js
@@ -0,0 +1,6 @@
+function areacn (){
+    var a  = document.getElementById("erea_cn_a").value;
+    var b  = document.getElementById("erea_cn_b").value;
+    document.getElementById("erea_cn_kq").value = a*b;
+
+}
