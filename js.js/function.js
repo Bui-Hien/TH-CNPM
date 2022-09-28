d0ad3e4f012e0175865db9a80fd30a54d8200944
Huyiii <hoangquochuy270103@gmail.com>
2022-09-29 01:19:10 +0700

    PTBac4

--- js.js/function.js
+++ js.js/function.js
@@ -31,3 +31,58 @@ function ketQua()
     
 }
 
+function ptBac4()
+{
+   
+    var a = document.getElementById("an4a").value;   
+    var b = document.getElementById("an4b").value;   
+    var c = document.getElementById("an4c").value;  
+    var d = document.getElementById("an4d").value;   
+    var f = document.getElementById("an4f").value;   
+    a = Number(a);
+    b = Number(b);
+    c = Number(c);
+    d = Number(d);
+    f = Number(f);
+    if(a != 0 && b==0 && d == 0)
+    {
+        delta = c*c - 4*a*f;
+        k = Math.sqrt(delta);
+        if(delta > 0)
+        {
+            t1 = (-c +k)/2*a;
+            t2 = (-c - k)/2*a;
+            if(t1 > 0)
+            {
+                document.getElementById("x1_4").value = Math.sqrt(t1);
+                document.getElementById("x2_4").value = -Math.sqrt(t1);
+            }else
+            {
+                document.getElementById("x1_4").value = "vô nghiệm";
+                document.getElementById("x2_4").value = "vô nghiệm";
+            }
+            if(t2 > 0)
+            {
+                document.getElementById("x3_4").value = Math.sqrt(t2);
+                document.getElementById("x4_4").value = -Math.sqrt(t2);
+            }else
+            {
+                document.getElementById("x3_4").value ="vô nghiệm";
+                document.getElementById("x4_4").value = "vô nghiệm";
+            }
+            
+        }else
+        {
+            document.getElementById("x1_4").value = "vô nghiệm";
+            document.getElementById("x2_4").value = "vô nghiệm";
+            document.getElementById("x3_4").value ="vô nghiệm";
+            document.getElementById("x4_4").value = "vô nghiệm";
+        }
+    }else
+    {
+        document.getElementById("x1_4").value = "vô nghiệm";
+        document.getElementById("x2_4").value = "vô nghiệm";
+        document.getElementById("x3_4").value ="vô nghiệm";
+        document.getElementById("x4_4").value = "vô nghiệm";
+    }
+}
